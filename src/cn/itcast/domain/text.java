daa34b054ae891d4f26849a8914994e50d3e6b77
Your Name <you@example.com>
2018-08-22 09:33:43 +0800

    Initial commit

---
+++ src/cn/itcast/domain/text.java
@@ -0,0 +1,14 @@
+package cn.itcast.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 影舞叶888
+ * @date 2018/8/21 23:12
+ */
+public class text {
+    List list = new ArrayList();
+    String[] sArray = new String[3];
+
+}
